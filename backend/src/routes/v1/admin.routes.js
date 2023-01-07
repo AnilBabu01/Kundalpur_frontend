9ecbbfa4d3136ee505961c1fb7bcdc2b99148b1b
rishadpt <rishadpt16@gmail.com>
2023-01-07 12:36:24 +0530

    feat:user management api's created

--- backend/src/routes/v1/admin.routes.js
+++ backend/src/routes/v1/admin.routes.js
@@ -10,7 +10,11 @@ router.route('/user-register').post(validate(userValidation.register), adminCont
 router.route('/donation-list').get(adminController.allList);
 router.route('/donation-list/:id').get(adminController.allList);
 router.route('/get-users').get(auth(),userController.getUsers)
+router.route('/get-users').put(auth(),adminController.editUser)
 router.route('/del-users').delete(auth(),adminController.delUser)
 router.route('/donation-type').post(auth(),adminController.addDonationType)
 router.route('/donation-type').get(auth(),adminController.getDonationType)
+router.route('/add-employee').post(auth(),adminController.addEmployees)
+router.route('/add-employee').get(auth(),userController.getEmployees)
+router.route('/add-employee').delete(auth(),userController.delEmployees)
 module.exports = router;

3a441b6a1234a139b4fec3ac818714e72ee9ccd5
rishadpt <rishadpt16@gmail.com>
2023-01-14 12:26:35 +0530

    commited

--- backend/src/utils/tokens.js
+++ backend/src/utils/tokens.js
@@ -27,7 +27,7 @@ const generateToken = (userId,role ,expires,secret = config.jwt.secret) => {
 const generateAuthTokens = async (user) => {
 
   let role = user?.adminDetails?.role_id || user?.roleDetails?.role_id 
-  const accessTokenExpires = moment().add(config.jwt.accessExpirationMinutes, 'minutes');
+  const accessTokenExpires = moment().add(config.jwt.accessExpirationMinutes, 'minutes')
   const accessToken = generateToken(user.id,role, accessTokenExpires);
   return {
     access: {
